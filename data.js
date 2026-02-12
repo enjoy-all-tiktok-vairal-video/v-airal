be4810a91da7d3559a5d3f722d4498d5c0215746
enjoy-all-tiktok-vairal-video <rajame331@gmail.com>
2026-02-13 03:42:15 +0600

    Update and rename video.html to data.js
    Signed-off-by: enjoy-all-tiktok-vairal-video <rajame331@gmail.com>

---
+++ data.js
@@ -0,0 +1,39 @@
+const videos = {
+
+"EA40":{
+title:"Episode 40",
+image:"https://i.ibb.co/8gjVZLtR/poster.jpg",
+final:"https://t.me/Enjoyallvideo_bot?start=a6c2bdd8"
+},
+
+"EA41":{
+title:"Episode 41",
+image:"https://i.ibb.co/8gjVZLtR/poster.jpg",
+final:"https://t.me/Enjoyallvideo_bot?start=de4394df"
+},
+
+"EA43":{
+title:"Episode 43",
+image:"https://i.ibb.co/8gjVZLtR/poster.jpg",
+final:"https://t.me/Enjoyallvideo_bot?start=c3d5601f"
+},
+
+"EA44":{
+title:"Episode 44",
+image:"https://i.ibb.co/8gjVZLtR/poster.jpg",
+final:"https://t.me/Enjoyallvideo_bot?start=cf9debf2"
+},
+
+"EA45":{
+title:"Episode 45",
+image:"https://i.ibb.co/8gjVZLtR/poster.jpg",
+final:"https://t.me/Enjoyallvideo_bot?start=867109a9"
+},
+
+"EA46":{
+title:"Episode 46",
+image:"https://i.ibb.co/8gjVZLtR/poster.jpg",
+final:"https://t.me/Enjoyallvideo_bot?start=57c59a8a"
+}
+
+};
